73a67a233fead60e410ccba3fce9c3baf55b6252
Ramazan Aslan <ramazanaslan21901@gmail.com>
2024-12-08 17:37:09 +0300

    Add support for array and json values

--- jn.h
+++ jn.h
@@ -1,4 +1,6 @@
+#include <assert.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #ifndef JN_H_
 #define JN_H_
@@ -30,14 +32,16 @@ typedef struct {
     int count;
 } JN_Obj;
 
-typedef struct {
+struct JN_Arr {
     struct JN_Arr** arrays;
     int arr_count;
     int arr_capacity;
     JN_Obj** items;
     int item_count;
     int item_capacity;
-} JN_Arr;
+};
+
+typedef struct JN_Arr JN_Arr;
 
 JN_Arr* jn_arr(char* json_data);
 JN_Obj* jn_obj(char* json_data);
@@ -46,86 +50,231 @@ void ja_free(JN_Arr* j);
 
 #endif // JN_H_
 
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+#define JN_IMPLEMENTATION
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+// DONOTPUSHTHISLINE
+
 #ifdef JN_IMPLEMENTATION
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-JN_Obj* jn_obj(char* json_data) {
+JN_Obj* jn_obj(char* json_data)
+{
     JN_Obj* jo = calloc(1, sizeof(JN_Obj));
 
-    strsep(&json_data, "{");
-    json_data = strsep(&json_data, "}");
-
-    char* sep;
-    while ((sep = strsep(&json_data, ",")) != 0) {
-        char* key = strsep(&sep, ":");
-        char* val = sep;
-        if (key[0] == '"') {
-            strsep(&key, "\"");
-            key = strsep(&key, "\"");
+    int start_idx = 0;
+    int end_idx = 0;
+    int stack = 0;
+    for (size_t i = 0; i < strlen(json_data); i++) {
+        char cur = json_data[i];
+        if (cur == '{') {
+            if (stack == 0) {
+                start_idx = i;
+            }
+            stack++;
         }
-        if (val[0] == '\"') {
-            strsep(&val, "\"");
-            val = strsep(&val, "\"");
+        if (cur == '}') {
+            stack--;
+            if (stack == 0) {
+                end_idx = i;
+            }
         }
-        memcpy(jo->items[jo->count].key, key, strlen(key));
-        mempcpy(jo->items[jo->count].value, val, strlen(val));
-        jo->count++;
     }
+
+    json_data[end_idx + 1] = '\0';
+    json_data += start_idx + 1;
+
+    int key_start = 0, key_end = 0;
+    int val_start = 0, val_end = 0;
+    int val_type = 0;
+
+    stack = 0;
+    int part = 0;
+    bool escaped = false;
+    for (size_t i = 0; i < strlen(json_data); i++) {
+        if (escaped) {
+            continue;
+        }
+        char cur = json_data[i];
+        if (cur == '\\') {
+            escaped = true;
+            continue;
+        }
+        if (cur == ' ') {
+            continue;
+        }
+        if (part == 0) {
+            if (cur == '"') {
+                if (stack == 0) {
+                    key_start = i + 1;
+                    stack = 1;
+                } else if (stack == 1) {
+                    stack = 0;
+                    part++;
+                    key_end = i;
+                    // printf("%.*s: ",
+                    //     key_end - key_start, json_data + key_start);
+                }
+            }
+        } else if (part == 1) {
+            val_start = i + 1;
+            val_end = i + 1;
+            if (cur == ':') {
+                part++;
+            }
+        } else if (part == 2) {
+            if (val_type != 0) {
+                if (val_type == 3 && (cur == ',' || cur == '}')) {
+                    val_end = i;
+                    // printf("%.*s\n",
+                    //     val_end - val_start, json_data + val_start);
+                    part = 0;
+                    val_type = 0;
+                    stack = 0;
+                    memcpy(jo->items[jo->count].key, json_data + key_start, key_end - key_start);
+                    mempcpy(jo->items[jo->count].value, json_data + val_start, val_end - val_start);
+                    jo->count++;
+                } else if (val_type == 2 && cur == '"') {
+                    val_end = i;
+                    // printf("%.*s\n",
+                    //     val_end - val_start, json_data + val_start);
+                    part = 0;
+                    val_type = 0;
+                    stack = 0;
+                    memcpy(jo->items[jo->count].key, json_data + key_start, key_end - key_start);
+                    mempcpy(jo->items[jo->count].value, json_data + val_start, val_end - val_start);
+                    jo->count++;
+                } else if (val_type == 1 && (cur == '}' || cur == ']')) {
+                    val_end = i + 1;
+                    // printf("%.*s\n",
+                    //     val_end - val_start, json_data + val_start);
+                    part++;
+                    val_type = 0;
+                    stack = 0;
+                    memcpy(jo->items[jo->count].key, json_data + key_start, key_end - key_start);
+                    mempcpy(jo->items[jo->count].value, json_data + val_start, val_end - val_start);
+                    jo->count++;
+                }
+            } else if (cur == '{' || cur == '[') {
+                val_type = 1;
+                val_start = i;
+                stack = 1;
+            } else if (cur == '"') {
+                val_type = 2;
+                val_start = i + 1;
+                stack = 1;
+            } else if (cur - '0' < 9) {
+                val_type = 3;
+                val_start = i;
+                stack = 0;
+            }
+        } else if (part == 3) {
+            if (cur == ',') {
+                part = 0;
+            }
+        }
+    }
+
     return jo;
 }
 
-void ja_add_obj(JN_Arr* ja, char* json_data) {
-    JN_Obj* jo = jn_obj(json_data);
+void ja_add_obj(JN_Arr* ja, char* json_data)
+{
+    JN_Obj* o = jn_obj(json_data);
     if (ja != NULL) {
         while (ja->item_count >= ja->item_capacity) {
             ja->item_capacity *= 2;
             ja->items = realloc(ja->items, sizeof(JN_Obj*) * ja->item_capacity);
         }
-        ja->items[ja->item_count++] = jo;
+        ja->items[ja->item_count++] = o;
     }
 }
 
-JN_Arr* jn_arr(char* json_data) {
+JN_Arr* jn_arr(char* json_data)
+{
     JN_Arr* ja = calloc(1, sizeof(JN_Arr));
 
-    bool is_array = false;
-
-    switch (json_data[0]) {
-    case '{': {
+    if (json_data[0] == '{') {
         ja->item_capacity = 1;
         ja->items = malloc(sizeof(JN_Obj*) * ja->item_capacity);
         ja->arrays = NULL;
-        break;
-    }
-    case '[': {
-        is_array = true;
-        ja->item_capacity = 8;
-        ja->items = malloc(sizeof(JN_Obj*) * ja->item_capacity);
-        ja->arr_capacity = 12;
-        ja->arrays = malloc(sizeof(JN_Arr*) * ja->item_capacity);
-        break;
-    }
-    default:
-        return NULL;
-    }
-
-    if (!is_array) {
         ja_add_obj(ja, json_data);
-    } else {
-        strsep(&json_data, "[");
-        json_data = strsep(&json_data, "]");
+        return ja;
+    }
 
-        char* sep;
-        while ((sep = strsep(&json_data, "}")) != 0) {
-            ja_add_obj(ja, sep);
+    ja->item_capacity = 8;
+    ja->items = malloc(sizeof(JN_Obj*) * ja->item_capacity);
+    ja->arr_capacity = 12;
+    ja->arrays = malloc(sizeof(JN_Arr*) * ja->item_capacity);
+
+    bool escaped = false;
+    int stack = 0;
+    int type = 0;
+    int item_start_idx = 0;
+    int item_end_idx = 0;
+    size_t length = strlen(json_data);
+    for (size_t i = 1; i < length; i++) {
+        if (escaped) {
+            continue;
+        }
+        char cur = json_data[i];
+        if (cur == '\\') {
+            escaped = true;
+            continue;
+        }
+        if (cur == ' ') {
+            continue;
+        }
+        if (cur == '{' || cur == '[') {
+            if (type == 0) {
+                if (cur == '{') {
+                    type = 1;
+                } else {
+                    type = 2;
+                }
+                item_start_idx = i;
+            }
+            stack++;
+        } else if (cur == ']' || cur == '}') {
+            stack--;
+            if (stack == 0) {
+                item_end_idx = i + 1;
+                if (type == 1 && cur == '}') {
+                    // printf("\ntype1: %.*s\n",
+                    //     item_end_idx - item_start_idx, json_data + item_start_idx);
+                    json_data[item_end_idx] = '\0';
+                    ja_add_obj(ja, json_data + item_start_idx);
+                } else if (type == 2 && cur == ']') {
+                    // printf("\ntype2: %.*s\n",
+                    //     item_end_idx - item_start_idx, json_data + item_start_idx);
+                    json_data[item_end_idx] = '\0';
+                    JN_Arr* ji = jn_arr(json_data + item_start_idx);
+                    ja->arrays[ja->arr_count++] = ji;
+                } else {
+                    // printf("\nassert: %.*s\n",
+                    //     item_end_idx - item_start_idx, json_data + item_start_idx);
+                    assert(false && "Invalid JSON format.");
+                }
+                type = 0;
+            }
         }
     }
+    printf("\n");
+
     return ja;
 }
 
-char* jo_value(JN_Obj* jo, const char* key) {
+char* jo_value(JN_Obj* jo, const char* key)
+{
     for (int i = 0; i < jo->count; i++) {
         if (strcmp(jo->items[i].key, key) == 0) {
             return jo->items[i].value;
@@ -134,9 +283,10 @@ char* jo_value(JN_Obj* jo, const char* key) {
     return NULL;
 }
 
-void ja_free(JN_Arr* ja) {
+void ja_free(JN_Arr* ja)
+{
     for (int i = 0; i < ja->arr_count; i++) {
-        free(ja->arrays[i]);
+        ja_free(ja->arrays[i]);
     }
     for (int i = 0; i < ja->item_count; i++) {
         free(ja->items[i]);
--- main.c
+++ main.c
@@ -2,45 +2,85 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #define JN_IMPLEMENTATION
 #include "jn.h"
 
-int main(void) {
-    char json[] = "{\"password\":9876543210123,\"companyName\":\"Company\",\"auth\":\"cXdlOjEyMw==\"}";
+/*
+ *  {}
+ *  [{}]
+ *  [{},{}]
+ *  [[],{}]
+ *  [[{}],{}]
+ *  [[{},{}],{},{}]
+ *  [[{},{}],[{},{}],{},{}]
+ */
 
-    // JN_Arr* j = jn_arr(json);
-    // JN_Obj* o = j->items[0];
-    JN_Obj* o = jn_obj(json);
+int main(void)
+{
+    char warehouse[] = "{\"id\":1,\"companyId\":1,\"name\":\"Warehouse1\",\"location\":{\"x\":0,\"y\":0},\"monthlyExpense\":120}";
+    (void)warehouse;
+    char warehouse_arr[] = "[{\"id\":1,\"companyId\":1,\"name\":\"Warehouse1\",\"location\":{\"x\":0,\"y\":0},\"monthlyExpense\":120}]";
+    (void)warehouse_arr;
+
+    // char json[] = "{\"password\":9876543210123,\"companyName\":\"Company\",\"auth\":\"cXdlOjEyMw==\"}";
+    //(void) json;
+
+    JN_Arr* j = jn_arr(warehouse);
+    JN_Obj* o = j->items[0];
+    // JN_Obj* o = jn_obj(warehouse);
 
     printf("---------------------------\n");
     for (int i = 0; i < o->count; i++) {
         printf("\"%s\": %s\n", o->items[i].key, o->items[i].value);
     }
     printf("---------------------------\n");
-    printf("%s -> %s\n", "auth", jo_value(o, "auth"));
-    printf("%s -> %s\n", "pass", jo_value(o, "password"));
-    printf("%s -> %s\n", "comp", jo_value(o, "comp"));
+    printf("%s -> %s\n", "id", jo_value(o, "id"));
+    printf("%s -> %s\n", "companyId", jo_value(o, "companyId"));
+    printf("%s -> %s\n", "name", jo_value(o, "name"));
+    printf("%s -> %s\n", "monthlyExpense", jo_value(o, "monthlyExpense"));
+    printf("%s -> %s\n", "location", jo_value(o, "location"));
+    printf("%s -> %s\n", "random", jo_value(o, "random"));
     printf("---------------------------\n");
 
-    char json_arr[] = 
-        "[{\"password\":1,\"companyName\":\"Neovim\",\"auth\":\"cXdlOjEyMw==\"},"
-        "{\"pass\":2,\"company\":\"Linux\",\"authkey\":\"38912*==\"},"
-        "{\"passkey\":3,\"comp\":\"XFCE\",\"token\":\"asjdkl==\"}]"
-    ;
+    JN_Obj* loc = jn_obj(jo_value(o, "location"));
+    for (int i = 0; i < loc->count; i++) {
+        printf("\"%s\": %s\n", loc->items[i].key, loc->items[i].value);
+    }
+    printf("---------------------------\n");
+
+    free(loc);
+    ja_free(j);
+
+    printf("\n\n*** *** *** *** *** *** ***\n\n");
+
+    char json_arr[] = "[{\"password\":1,\"companyName\":\"Neovim\",\"auth\":\"cXdlOjEyMw==\"},"
+                      "{\"pass\":2,\"company\":\"GNU\",\"authkey\":\"38912*==\"},"
+                      "[{\"pass\":3,\"company\":\"Linux\",\"authkey\":\"asdjlkasdjlk=\"},{\"pass\":4,\"company\":\"Linux\",\"authkey\":\"98ad0*sdsaj\"}],"
+                      "{\"passkey\":[1,2,3,4,5],\"comp\":\"XFCE\",\"token\":\"asjdkl==\"}]";
 
     JN_Arr* ja = jn_arr(json_arr);
 
-    for (int i = 0; i < ja->item_count - 1; i++) {
+    for (int i = 0; i < ja->item_count; i++) {
         JN_Obj* jo = ja->items[i];
         for (int k = 0; k < jo->count; k++) {
             printf("\"%s\": %s\n", jo->items[k].key, jo->items[k].value);
         }
         printf("---------------------------\n");
     }
-    // ja_free(j);
+
+    printf("ARRAYS\n");
+    for (int i = 0; i < ja->arr_count; i++) {
+        JN_Arr* ji = ja->arrays[i];
+        for (int k = 0; k < ji->item_count; k++) {
+            JN_Obj* jo = ji->items[k];
+            for (int j = 0; j < jo->count; j++) {
+                printf("\"%s\": %s\n", jo->items[j].key, jo->items[j].value);
+            }
+            printf("---------------------------\n");
+        }
+    }
+
     ja_free(ja);
-    free(o);
     return 0;
 }
